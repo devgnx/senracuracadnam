b4036d7562a1e127f865a04ed5c840ddb1ca357e
André Gonçalves <dev.andregnx@gmail.com>
2016-09-11 16:52:43 -0300

    Added Contact Controller

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,35 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Contact;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Traits\LayoutResolver;
-
 class ContactController extends Controller
 {
-    use LayoutResolver;
+    use Traits\LayoutResolver;
 
-    protected $page  = 'contact';
-    protected $title = 'Dados de Contato';
-
-    public function edit()
+    public function index()
     {
-        $this->addVar(['contact' => Contact::first()]);
-        return view('admin.contact.form', $this->compactVars());
-    }
-
-    public function save(Request $request)
-    {
-        $contact = Contact::firstOrNew(['id' => 1]);
-        $contact->telephone = $request->input('telephone');
-        $contact->email     = $request->input('email');
-        $contact->postal_code = $request->input('postal_code');
-        $contact->address = $request->input('address');
-        $contact->city    = $request->input('city');
-        $contact->state   = $request->input('state');
-
-        if ($contact->save()) {
-          return redirect()->route('admin::contact:edit')->with('success', ["Dados salvos com sucesso!"]);
-        }
+        return view("contact", $this->compactVars());
     }
 }
